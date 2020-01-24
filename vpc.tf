38b2ed2c0b349f8acce15bb7e0c305847474433e
will-au <50462310+will-au@users.noreply.github.com>
2020-01-24 14:26:55 -0800

    Create vpc module
    Added Gaurav's VPC Module

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "gaurav-vpc"
+  cidr = "10.0.0.0/24"
+
+  azs             = ["us-west-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
