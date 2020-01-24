e03b3a3063d9cdc8616ce547f801e7f3194bc1c5
will-au <50462310+will-au@users.noreply.github.com>
2020-01-24 14:34:48 -0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "gaurav-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1d", "us-east-1f", "us-east-1e"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
